5261deb117203dd48d725d392c4a0b7791980e70
nasir-buddy <syedali172000@gmail.com>
2023-06-05 02:05:38 +0530

    Types in js

---
+++ Java_script/type.js
@@ -0,0 +1,14 @@
+var a = null;
+if(a){
+    console.log("True");
+} else {
+    console.log("False");
+}
+// if(a > 0)
+// {
+//     console.log("Positive");
+// } else if(a < 0){
+//     console.log("Negative");
+// } else {
+//     console.log("Zero");
+// }
