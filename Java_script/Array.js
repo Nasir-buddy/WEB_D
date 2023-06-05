488484d282dfde5b4af5f2a88a12db7bd2992d7f
nasir-buddy <syedali172000@gmail.com>
2023-06-05 16:49:06 +0530

    JS: arrays in js

---
+++ Java_script/Array.js
@@ -0,0 +1,8 @@
+var arr = [1 , 2 ,3 ,4, 5];
+console.log(arr.length);
+console.log(arr[3]);
+console.log(arr.pop());
+console.log(arr.push(78));
+console.log(arr)
+console.log(arr.shift(0));
+console.log(arr);
