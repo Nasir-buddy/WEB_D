e53c34324dd90371457842b61f5c1e38b1c65c09
nasir-buddy <syedali172000@gmail.com>
2023-06-05 15:52:11 +0530

    changes in function

--- Java_script/function.js
+++ Java_script/function.js
@@ -1,6 +1,35 @@
-function sum(a , b){
-    // console.log(a + b);
-    return a + b;
+// function sum(a , b){
+//     // console.log(a + b);
+//     return a + b;
+// }
+// var result = sum(10 , 3, 32);
+// console.log(result);        
+
+// function hoisetDemo(){
+//     console.log(i);
+//     var i = 10;
+// }
+// hoisetDemo();
+// var name = "Global";
+// // console.log(name);
+
+// function scopeDemo() {
+//     var name = "Function";
+//     console.log(name);
+// }
+// scopeDemo();
+// console.log(name);
+// console.log(i);
+function c(){
+    console.log("Inside c");
 }
-var result = (10 + 10);
-console.log(result);
+function b(){
+    c();
+    console.log("Inside b");
+}
+function a(){
+    b();
+    console.log("Inside a");
+}
+a();
+console.log("Global context.")
