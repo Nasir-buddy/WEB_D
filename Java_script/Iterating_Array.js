ec8d78bfcf40618f7a4d433ae2f56eb08ad76cc8
nasir-buddy <syedali172000@gmail.com>
2023-06-06 15:51:03 +0530

    JS: Iterating on array

---
+++ Java_script/Iterating_Array.js
@@ -0,0 +1,9 @@
+var arr = [1 ,2 , 3,4 ,5];
+function print(element){
+    console.log(element);
+}
+// for(var i = 0 ; i < arr.length; i++){
+//     print(arr[i]);
+//     // console.log(arr[i]);
+// }
+arr.forEach(print); 
