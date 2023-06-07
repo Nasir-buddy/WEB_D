ba542967a63f6fde21f7c0264321706204071c3c
nasir-buddy <syedali172000@gmail.com>
2023-06-07 23:40:56 +0530

    JS : stoping interval in JS

--- Java_script/Object_in_JS/Timing_event.js
+++ Java_script/Object_in_JS/Timing_event.js
@@ -2,9 +2,15 @@ var second = 1;
 function sayHello(){
     console.log("After", second);
     second++;
+    if(second == 6){
+        // stop
+        clearInterval(id);
+    }
 }
 // sayHello is making a function and 2000 is time in milli second which denote that the 
 // function execute after that time.
 
 // setTimeout(sayHello, 2000);
-setInterval(sayHello, 1000);
+// setInterval(sayHello, 1000);
+
+var id = setInterval(sayHello, 1000)
