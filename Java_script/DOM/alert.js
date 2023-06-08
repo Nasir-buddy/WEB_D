69d7a1a101fb3787f1c7a35a0753cfd409d0612d
nasir-buddy <syedali172000@gmail.com>
2023-06-09 00:50:07 +0530

    JS: second button

--- Java_script/DOM/alert.js
+++ Java_script/DOM/alert.js
@@ -1,4 +1,10 @@
-var helloButton = document.getElementById('btn');
-helloButton.addEventListener('click' , function(){
+function sayHello(){
     alert('Hello');
-});
+}
+function sayBye(){
+    alert('Byee!!')
+}
+var helloButton = document.getElementById('btn1');
+
+helloButton.addEventListener('click', sayBye);
+helloButton.addEventListener('click', sayHello);
