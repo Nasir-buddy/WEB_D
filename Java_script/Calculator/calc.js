be2fbd40271f041e1abd72cc384d85fde2280207
nasir-buddy <syedali172000@gmail.com>
2023-06-12 02:52:54 +0530

    Assingment : write js code of calc

--- Java_script/Calculator/calc.js
+++ Java_script/Calculator/calc.js
@@ -1,7 +1,55 @@
-console.log(eval(2 + 3));
-var op = 2;
-var op2 = 3;
+var buttons = document.getElementsByClassName("button");
+var display = document.getElementById("display");
 
-var operand = '*';
-var result = eval(op + " " + operand + " " + op2);
-console.log(result);
+var operand1 = 0; 
+var operand2 = null;
+var operand3 = null;
+
+function isOperator(value){
+    return value == "+" || value == "-" || value == "*" || value == "/";
+}
+
+for(var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function(){
+        var value = this.getAttribute('data-value');
+        var text = display.textContent.trim();
+
+        if(isOperator(value)){
+            operator = value;
+            operand1 = parseFloat(text);
+            display.textContent = "";
+        } else if(value == "ac"){
+            display.textContent = "";
+        } else if(value == "sign"){
+            operand1 = parseFloat(text);
+            operand1 = -1 * operand1;
+            display.textContent = operand1;
+        } else if(value == "."){
+            if(text.length && !text.includes('.')){
+                display.textContent = text + '.';
+            }
+        } else if(value == "%"){
+            operand1 = parseFloat(text);
+            operand1 = operand1 / 100;
+            display.textContent = operand1;
+        }
+        // else if(value == '+'){
+        //     operand1 = parseFloat(text);
+        //     operand2 = parseFloat(text);
+        //     operand3 = eval(operand1 + operand2);
+        //     display.textContent = operand3;
+        // }
+         else if(value == "="){
+            operand2 = parseFloat(text);
+            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+            if(result){
+                display.textContent = result;
+                operand1 = result;
+                operand2 = null;
+                operator = null;
+            }
+        }else{
+            display.textContent += value;
+        }
+    })
+}
