9383fb136414cec9c3daaf4696f24b693ca5c08d
nasir-buddy <syedali172000@gmail.com>
2023-06-13 15:59:36 +0530

    JS: Let variable

--- Java_script/Letinjs.js
+++ Java_script/Letinjs.js
@@ -1,8 +1,15 @@
+console.log(a);
 var a = 10;
+
 if(a <= 10){
-    let b = 40;
-    // console.log(b)
-} else {
-    let c = 50;
+    console.log(b);
+    let b = 20;
 }
-console.log(a , b , c);
+// var a = 10;
+// if(a <= 10){
+//     let b = 40;
+//     // console.log(b)
+// } else {
+//     let c = 50;
+// }
+// console.log(a , b , c);
