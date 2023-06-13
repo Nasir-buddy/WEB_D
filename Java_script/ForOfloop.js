b7b77130439fe95bdd64843995d1b0915a582b66
nasir-buddy <syedali172000@gmail.com>
2023-06-13 16:39:17 +0530

    While in js

--- Java_script/ForOfloop.js
+++ Java_script/ForOfloop.js
@@ -2,7 +2,12 @@ var arr = [1, 2, 3, 4 ,5 ];
 for(let i = 0; i < arr.length; i++){
     console.log(arr[i]);
 }
-
+let i = 0;
+while(i < arr.length){
+    console.log(arr[i]);
+    i++;
+}
 for(let i of arr){
     console.log(i);
 }
+
