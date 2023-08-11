561894043a8c8e84426feec4e311c2ad7911d06d
nasir-buddy <syedali172000@gmail.com>
2023-08-11 14:41:14 +0530

    js: truthy and falsy

--- Java_Script_basic/hoisiting.js
+++ Java_Script_basic/hoisiting.js
@@ -1,13 +1,8 @@
-var a = [12 , 13];
-var b = a;
-b.pop();
-console.log(a);
-
 // loops
-
-for(var i = 0; i < 5 ; i++){
-    for(var j = 0; j < i; j++){
-        console.log("*");
+var ans = "";
+for(let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        ans += "*";
     }
-    console.log();
-}
+    console.log(ans);
+}   
---
+++ Java_Script_basic/truthy_Falsy.js
@@ -0,0 +1,8 @@
+// js me kuch bhi likho wo mainly do prakaar mein se kisi ek prakaar ko belong karti hai .
+// falsy is = to 0 , false , undefined null nan and document.all.
+
+ if(2){
+    console.log("nasir");
+ } else {
+    console.log("ali");
+ }
