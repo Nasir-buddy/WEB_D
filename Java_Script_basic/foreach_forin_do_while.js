5ba7f9f888ebcf4419803c962ff63b377f1c97a6
nasir-buddy <syedali172000@gmail.com>
2023-08-11 14:54:53 +0530

    for in loop in js

--- Java_Script_basic/foreach_forin_do_while.js
+++ Java_Script_basic/foreach_forin_do_while.js
@@ -4,3 +4,15 @@ var a = [1,2,3,4,5,6,7,8,9,];
 a.forEach(function(val){
     console.log(val + 2);
 })
+console.log(a);
+
+// forin
+// objests par lop karne ke liye hota hai foin loop 
+var obj = {
+    name: "Nasir Ali",
+    age: 23,
+    city: "bhopal"
+}
+for(var iterate in obj){
+    console.log(iterate , obj[iterate]);
+}
