83fb6edb6239783e23147170fa3f656997902126
nasir-buddy <syedali172000@gmail.com>
2023-08-16 20:52:26 +0530

    add html file to our server

--- Backend/BasicHttpserver/server.js
+++ Backend/BasicHttpserver/server.js
@@ -1,10 +1,20 @@
 const http = require("node:http");
 const port = 8000;
+// fs is use to read and writing into from a files.
+const fs = require('fs');
 // request handler
 function requestHandler(request, response){
-    console.log(request.url);
-    response.end("hello")
-}
+    // console.log(request.url);
+    response.writeHead(200, {'contect-type': 'text/html'});
+    //fs = file system
+    fs.readFile('./index.html', function(err , data){
+        if(err){
+            console.log("erroe",err);
+            return response.end('<h1>Error!</h1>');
+        }
+        return response.end(data);
+    });
+};
 const server = http.createServer(requestHandler);
 
 server.listen(port , function(err){
