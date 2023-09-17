3d2a1a0c4c7a5ca65f4e0c7a6a793ef1312f8339
nasir-buddy <syedali172000@gmail.com>
2023-09-17 15:21:30 +0530

    more on async js

---
+++ Async JS/async.js
@@ -0,0 +1,7 @@
+// async code kuch der bad chalega jbbhi chalega wo callback ko chala dega
+console.log("hey")
+
+setTimeout(() => { // known as call fns.. because it will run after getting answer
+    console.log("hey2")
+}, 2000);
+console.log("chut")
