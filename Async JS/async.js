5c37f82f60c04c1d590d81d4657cbcb0706d62fb
nasir-buddy <syedali172000@gmail.com>
2023-09-21 20:32:19 +0530

    promises

--- Async JS/async.js
+++ Async JS/async.js
@@ -5,8 +5,9 @@ console.log("hey")
 setTimeout(() => { // known as call fns.. because it will run after getting answer
     console.log("hey2")
 }, 2000);
-console.log("chut")
+// console.log("hi")
 
+//async ki poori kahani
 // Js is not asynchronous 
 // there is two thing 
 // 1st is main stack 
@@ -15,6 +16,7 @@ console.log("chut")
 // aur jo bhi side stack pe hota hai wo begind the scenes procerssing kr sakta hai aur jab
 // uski processing complete ho use main stack me la ke chalaya ja sakta hai 
 
+// single threading and multi threading
 // sync code always runs on main stack 
 console.log("hey 1");
 console.log("hey 2");
@@ -23,4 +25,54 @@ setTimeout(()=>{
     console.log("hey 3");
 }, 0);
 console.log("hey 4");
-//async ki poori kahani
+
+// callbacks
+// call back hamesa ek function hota hai , ye sirf tab chalta hai jab asynch code ka completion hojata hai.
+// async code likhne ke liye 
+// fetch
+// XMLHttpRequest
+// axios
+//  promises
+// settimeout
+// setinterval
+// inka ans chalane ke liye hota hai callback
+// ''          ''            ''      then catch
+//  ''        ''             ''       async await
+
+//  fetch
+    // axios
+    // promises
+// var ans = new Promise((res, rej)=>{
+//         if(false){
+//             return res();
+//         } else {
+//             return rej();
+//         }
+//     });
+//     ans
+//         .then(()=>{
+//             console.log("resolve ho gya tha.");
+//         })
+//         .catch(()=>{
+//             console.log("resolve nhi hua");
+//         });
+var ans = new Promise((res, rej)=>{
+    var n = Math.floor(Math.random() * 10);
+    if(n < 5){
+        return res();
+    } else {
+        return rej();
+    }
+})
+ans.then(()=>{
+    console.log("below 5");
+})
+.catch(()=>{
+    console.log("not resolve")
+})
+    // settimeout
+    // setinterval
+
+    // then catch
+    // callback
+    // async await
