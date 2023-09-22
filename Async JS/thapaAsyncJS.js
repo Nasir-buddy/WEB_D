1b99060109a16d30234d68147fa6360357b51682
nasir-buddy <syedali172000@gmail.com>
2023-09-22 12:06:08 +0530

    more on promises

--- Async JS/thapaAsyncJS.js
+++ Async JS/thapaAsyncJS.js
@@ -15,13 +15,35 @@
 const pobj1 = new Promise((res, rej) => {
     setTimeout(() => {
         let roll_no = [1, 2, 3, 4, 5];
-        // return res(roll_no);
-        return rej("nothing to pass");
+        return res(roll_no);
+        // return rej("nothing to pass");
     }, 2000);
 });
+
+const getBiodata = (indexData) => {
+    return new Promise((res, rej) => {
+        setTimeout((indexData) => {
+            let getBiodata = {
+                name: "Nasir Ali",
+                age: 22
+            }
+            return res(`My roll no is ${indexData}. My name is ${getBiodata.name} and I am ${getBiodata.age} old.`);        
+        }, 2000, indexData)
+    })
+}
+
+
 pobj1.then((roll_no)=>{
     console.log(roll_no);
+    getBiodata(roll_no[1]).then((anything) => {
+        console.log(anything);
+    })
 })
 .catch((rej)=>{
     console.log("not resolved",rej);
-})
+});
+
+
+
+
+
