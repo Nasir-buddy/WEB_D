023392b7244e96aed113cbab78edf8ded42c58e9
nasir-buddy <syedali172000@gmail.com>
2023-10-06 16:45:24 +0530

    Express js and routing

--- Backend_SHC/script.js
+++ Backend_SHC/script.js
@@ -8,17 +8,32 @@
 // npm i i = install both can be usable 
 // use
 // how to print results
-var oneLineJoke = require('one-liner-joke');
-var figlet = require('figlet');
+// var oneLineJoke = require('one-liner-joke');
+// var figlet = require('figlet');
 
-console.log(oneLineJoke.getRandomJoke());
+// console.log(oneLineJoke.getRandomJoke());
 
-figlet("Nasir Ali", function(err, data){
-    if(err){
-        console.log("Something went wrong...");
-        console.log(err);
-        return;
-    } 
-    console.log(data);
-})
+// figlet("Nasir Ali", function(err, data){
+//     if(err){
+//         console.log("Something went wrong...");
+//         console.log(err);
+//         return;
+//     } 
+//     console.log(data);
+// })
 
+
+const exp = require('express');
+const app = exp();
+const port = 3000;
+console.log("Server is started successfully on port :", port);
+app.get('/', (req, res)=>{
+    res.send("hey me");
+});
+app.get('/profile', (req, res)=>{
+    res.send("hey this is profile page.");
+});
+app.get('/like', (req, res)=>{
+    res.send("hey like me.");
+});
+app.listen(port);
