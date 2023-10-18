e3c7ad9fb0befb0d99b47b7a24c8c6c4556c84f7
Nasir-Buddy <syedali172000@gmail.com>
2023-10-18 18:37:26 +0530

    Express js revise

---
+++ Backend_SHC/express/script.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    // console.log(req);
+    console.log("Middle ware");
+    next();
+});
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+app.get('/profile', (req, res) => {
+    res.send('profile section');
+})
+app.get('/home', (req, res) => {
+    res.send('home section');
+})
+app.get('/about', (req, res) => {
+    res.send('about section');
+})
+
+app.listen(8000);
