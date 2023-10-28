aeded35c7127a0a3bd930bdfda48c035e238e615
Nasir-Buddy <syedali172000@gmail.com>
2023-10-28 23:13:29 +0530

    fix css

--- codeial/controllers/user_controller.js
+++ codeial/controllers/user_controller.js
@@ -1,5 +1,5 @@
 module.exports.profile = (req, res)=>{
-    return res.render('users.ejs',{
+    return res.render('users_profile.ejs',{
         title: "Entered in users directory"
     })
 }
--- codeial/index.js
+++ codeial/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 
+// importing the express layouts in the project
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static(__dirname + '/assets'));
+
+app.use(expressLayouts);
 
 // use express router
 app.use('/', require('./routes'));
@@ -11,7 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 // resuming learning
-app.listen(port, (err)=>{
+app.listen(port, (err)=>{     
     if(err){
         console.log(`Error in running the server: ${err}`);
     }
