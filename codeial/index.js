6c15a20eb2658c03f12f4884ef1de618dc1be56c
Nasir-Buddy <syedali172000@gmail.com>
2023-10-29 00:51:50 +0530

    setup static files access for layout and pages

--- codeial/index.js
+++ codeial/index.js
@@ -9,6 +9,9 @@ app.use(express.static(__dirname + '/assets'));
 
 app.use(expressLayouts);
 
+//extract stryle and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 // use express router
 app.use('/', require('./routes'));
 
