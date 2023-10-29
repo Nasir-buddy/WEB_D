beac29a040047f35e76fb206f7458b3b9abdc7c9
Nasir-Buddy <syedali172000@gmail.com>
2023-10-29 15:41:08 +0530

    creater user sign up

--- codeial/controllers/user_controller.js
+++ codeial/controllers/user_controller.js
@@ -1,5 +1,47 @@
+const User = require('../models/user')
+
 module.exports.profile = (req, res)=>{
     return res.render('users_profile.ejs',{
         title: "Entered in users directory"
     })
 }
+
+//render the sign up page 
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    })
+}
+
+// render the sign in page
+module.exports.singIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = async (req, res)=>{
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('/users/sign-in');
+    }
+    try{
+        const existingUser = await User.findOne({email: req.body.email});
+        if(!existingUser){
+            const user = await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        } else {
+            return res.redirect('back');
+        }
+    } catch {
+        console.error("Error in finding/creating user while signing up", error);
+        return res.redirect('back');
+    }
+};
+
+
+// sign in and get the create session 
+module.exports.createSession = function(req, res){
+    // TODO later
+}
+
--- codeial/routes/users.js
+++ codeial/routes/users.js
@@ -4,5 +4,9 @@ const app = express.Router();
 const userController = require('../controllers/user_controller');
 
 app.get('/profile', userController.profile);
+app.get('/sign-up', userController.signUp);
+app.get('/sign-in', userController.singIn);
+
+app.post('/create', userController.create);
 
 module.exports = app;
