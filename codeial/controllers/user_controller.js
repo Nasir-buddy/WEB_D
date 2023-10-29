f4a216ba5ba543af09dec12a44afca6d8f3d3389
Nasir-Buddy <syedali172000@gmail.com>
2023-10-29 16:10:10 +0530

    manual authentication

--- codeial/controllers/user_controller.js
+++ codeial/controllers/user_controller.js
@@ -22,26 +22,49 @@ module.exports.singIn = function(req, res){
 
 //get the sign up data
 module.exports.create = async (req, res)=>{
+    // checking the password is same or not if not then return to sign in page 
     if(req.body.password != req.body.confirm_password){
         return res.redirect('/users/sign-in');
     }
+    // other wise we try to create new user
     try{
+        // finding the user id by email
         const existingUser = await User.findOne({email: req.body.email});
+        // if the user is not existing then 
         if(!existingUser){
+            // creating the user 
             const user = await User.create(req.body);
+            // and redirect to rhe sign in page
             return res.redirect('/users/sign-in');
         } else {
+            // otherwise returning back to the main page 
             return res.redirect('back');
         }
     } catch {
+        // catching the error in the creating contact 
         console.error("Error in finding/creating user while signing up", error);
+        //and returing to the main page 
         return res.redirect('back');
     }
 };
 
 
 // sign in and get the create session 
-module.exports.createSession = function(req, res){
-    // TODO later
-}
+module.exports.createSession = async (req, res)=>{
+   try{
+    const user = await User.findOne({email: req.body.email});
 
+    if(user){
+        if(user.password != req.body.password){
+            return res.redirect('back');
+        }
+        res.cookie('user_id', user.id);
+        return res.redirect('/users/profile');
+    } else {
+        return res.redirect('back');
+    }
+   } catch (err){
+    console.err('Error in finding user in signing in.', err);
+    return res.redirect('back');
+   }
+};
