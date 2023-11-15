78ee9758b1d8b3ac1c2e417073c02ab4b12fcd2a
Nasir-Buddy <syedali172000@gmail.com>
2023-11-15 14:27:28 +0530

    [MongoDB]: search by categories

--- Backend_SHC/app-2/routes/index.js
+++ Backend_SHC/app-2/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel = require('./users');
+const { render } = require('../app');
+
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index');
 });
+
+// router.get('/failed', function(req, res) {
+//   // flash msg aapko allow krte hai nki aap is orute me bane hue data do dushre route me use kr skte hain 
+//   req.flash('age', 12);
+//   req.flash('name', "Nasir Ali");
+//   res.send("ho gya bn gya");
+// });
+
+// router.get('/check', function(req, res) {
+//   console.log(req.flash('age'), req.flash('name'));
+//   res.send('check kr lo bck ke terminal par')
+//   });
+
+// Mongoose question
+router.get("/create", async (req, res) => {
+  let userData = await userModel.create({
+    username: "Monu",
+    nickname: "Sim ",
+    description: 'I am Samreen  and i love drawing',
+    categories: {
+      type: Array,
+      default: ['math pro']
+    }
+  })
+  res.send(userData);
+});
+
+router.get('/find', async(req, res)=>{
+  // new RedExp(search , flag)
+  // var regex = new RegExp('^NaSiR$', 'i')
+  // let user = await userModel.find({username: regex});
+  console.log(res);
+  var regex = new RegExp("^samreen$", 'i')
+  let user = await userModel.find({categories: { $all :
+     ["drawing", "node"]}});
+
+  
+  console.log(user);
+  res.send(user);
+})
 
 module.exports = router;
